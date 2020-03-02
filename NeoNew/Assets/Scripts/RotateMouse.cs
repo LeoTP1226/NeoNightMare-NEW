94f7622c8d26c3cdfb710b7b00a6c20096af716b
Leo <pupdoodle@gmail.com>
2020-03-02 14:52:07 +1300

    Textures and main coding done

---
+++ NeoNew/Assets/Scripts/RotateMouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMouse : MonoBehaviour
+{
+    public float movespeed = 1.0f;
+    public Rigidbody rb;
+
+    public Vector3 moveInput;
+    public Vector3 moveVelocity;
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * movespeed;
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+           transform.LookAt(pointToLook);
+        }
+        /*Vector3 mousePos = Input.mousePosition;
+        Debug.Log(mousePos.x);
+        Debug.Log(mousePos.y);
+    */
+    }
+}
