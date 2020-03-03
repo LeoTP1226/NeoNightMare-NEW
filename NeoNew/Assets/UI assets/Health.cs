7bc53cb7ddaa22fe0599ac170c97badd15aa93a2
Leo <pupdoodle@gmail.com>
2020-03-03 13:41:07 +1300

    UI heart not working

---
+++ NeoNew/Assets/UI assets/Health.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image [] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+
+            }
+        }
+    }
+    public int CharaHealth = 3;
+    public float speed;
+
+    private bool isDead = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            EnemyDamage();
+        }
+
+    }
+
+    void EnemyDamage()
+    {
+        CharaHealth -= 1;
+        numOfHearts -= 1;
+
+        if (CharaHealth <= 0)
+        {
+            Death();
+            health -= 1;
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        Destroy(gameObject);
+    }
+}
