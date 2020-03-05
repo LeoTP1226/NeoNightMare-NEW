bf39bcae39198ab2ec3b5b78770a39f7800f1b70
Em <elbaunton@gmail.com>
2020-03-05 15:04:49 +1300

    Added game over scene

---
+++ NeoNew/Assets/Scripts/pause2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause2 : MonoBehaviour
+{
+
+    private bool isPaused = true;
+    public GameObject pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void OnQuit()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
