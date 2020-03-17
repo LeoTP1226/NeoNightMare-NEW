0681e1441cdf0e4a774dc739010159b71e9bce1b
leo <pupdoodle@gmail.com>
2020-03-17 15:30:30 +1300

    TuesEnd just need rotate script

--- NeoNew/Assets/Scripts/Old/EnemyFollow.cs
+++ NeoNew/Assets/Scripts/Old/EnemyFollow.cs
@@ -22,37 +22,37 @@ public class EnemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Vector3 direction = player.transform.position - transform.position;
+      // Vector3 direction = player.transform.position - transform.position;
       //  float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //rb.rotation = angle; 
-        direction.Normalize();
-        movement = direction;
+        //direction.Normalize();
+       // movement = direction;
         //moveCharacter(movement);
         //moveCharacter(Vector3 direction);
     
        
         
 
-       // Vector3 targetDirection = player.transform.position - transform.position;
+        Vector3 targetDirection = player.transform.position - transform.position;
          float singleStep = speed * Time.deltaTime;
          Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, singleStep, 0.0f);
          Debug.DrawRay(transform.position, newDirection, Color.red);
          transform.rotation = Quaternion.LookRotation(newDirection * 180.0f);
 
-        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+       // rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
 
 
 
     }
 
-   /* private void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 direction = player.transform.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
 
     }
-    */
+    
     
 
    
