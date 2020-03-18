5953ae31c50a414a7f8a22c1496f895b338aa0a2
leo <pupdoodle@gmail.com>
2020-03-18 14:02:12 +1300

    Revert "preparing for last script"
    This reverts commit dd058c36e680f79b816c09aa3b1c41f21370fb8b.

--- NeoNew/Assets/Scripts/Old/EnemyFollow.cs
+++ NeoNew/Assets/Scripts/Old/EnemyFollow.cs
@@ -22,7 +22,7 @@ public class EnemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Vector3 direction = player.transform.position - transform.position;
+      // Vector3 direction = player.transform.position - transform.position;
       //  float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //rb.rotation = angle; 
         //direction.Normalize();
